02696429aded3ad6ba2db555e9f5a8fd7d0cfdea
xupan <xupan@haodf.com>
2016-09-05 10:39:36 +0800

    初始化工程

---
+++ Life/Life/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Life
+//
+//  Created by 徐攀 on 16/9/5.
+//  Copyright © 2016年 iDustpan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
